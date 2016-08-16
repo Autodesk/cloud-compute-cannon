e749667c65c4e12269152ac46e8f410474afe4b0
Dion Amago <dion@transition9.com>
2016-08-16 16:42:12 -0700

    Fix imports, the test import.hx is symlinked

--- src/haxe/ccc/compute/import.hx
+++ src/haxe/ccc/compute/import.hx
@@ -1,2 +1,13 @@
 import ccc.compute.Definitions;
 import ccc.compute.Constants.*;
+
+import haxe.Json;
+import promhx.deferred.*;
+import promhx.Promise;
+import t9.abstracts.time.*;
+import t9.abstracts.net.*;
+import t9.util.ColorTraces.*;
+
+import t9.util.ColorTraces.*;
+using StringTools;
+using Lambda;
