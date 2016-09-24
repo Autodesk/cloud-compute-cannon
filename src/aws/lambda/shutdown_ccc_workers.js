66c027c9b201d11286bd55716fce484ebcdc8caf
Dion Amago <dion@transition9.com>
2016-09-23 21:05:03 -0700

    Fix lambda worker cleanup

--- src/aws/lambda/shutdown_ccc_workers.js
+++ src/aws/lambda/shutdown_ccc_workers.js
@@ -1,23 +1,26 @@
 'use strict';
 
 //Take this out when running in the cloud
-// require('dotenv').config();
-// console.log(process.env);
+var LOCAL = false;
+
+if (LOCAL) {
+    require('dotenv').config();
+}
 
 const AWS = require('aws-sdk');
-//Take this out when running in the cloud
-// AWS.config.update({region: 'us-west-1'});
-
-
+if (LOCAL) {
+    AWS.config.update({region: 'us-west-2'});
+}
 
 var ec2 = new AWS.EC2();
 
 function getInstanceData(instances, instanceId) {
     for (var i = 0; i < instances.Reservations.length; i++) {
-        var instanceData = instances.Reservations[i];//Because we only ever create a single instance per API call.
-        instanceData = instanceData.Instances[0];
-        if (instanceData.InstanceId == instanceId) {
-            return instanceData;
+        for (var j = 0; j < instances.Reservations[i].Instances.length; j++) {
+            var instanceData = instances.Reservations[i].Instances[j];
+            if (instanceData.InstanceId == instanceId) {
+                return instanceData;
+            }
         }
     }
     return null;
@@ -29,7 +32,7 @@ function checkWorker(workerId, ownerId, instances, cb) {
     console.log('Found worker=' + workerId + ' with owner=' + serverName);
     if (serverData === null || serverData.State.Name != 'running') {
         console.log('Server not running for worker=' + workerId + ' server=' + serverName);
-        ec2.terminateInstances({InstanceIds:[workerId]}, (err, data) => {
+        ec2.terminateInstances({InstanceIds:[workerId], DryRun: true}, (err, data) => {
             if (err) {
                 console.error(err);
             } else {
@@ -43,14 +46,11 @@ function checkWorker(workerId, ownerId, instances, cb) {
 }
 
 exports.handler = function(event, context, callback) {
-    console.log('hander event=', event);
-    console.log('hander context=', context);
-    ec2.describeInstances({Filters: [{Name: 'tag-key', Values: ['CCC_TYPE']}]}, (err, instances) => {
+    ec2.describeInstances(null, (err, instances) => {
         if (err) {
             console.log('Failed to run ec2.describeInstances', err);
             console.error(err);
         } else {
-            console.log(instances);
             var workersToCheck = [];
             for (var i = 0; i < instances.Reservations.length; i++) {
                 var instanceData = instances.Reservations[i];//Because we only ever create a single instance per API call.
@@ -78,15 +78,23 @@ exports.handler = function(event, context, callback) {
                                 workersToCheck.splice(index, 1);
                             }
                             if (workersToCheck.length === 0) {
-                                callback(null, "Done");
+                                if (callback !== undefined) {
+                                    callback(null, "Done");
+                                }
                             }
                         });
                     }
                 }
             }
             if (workersToCheck.length === 0) {
-                callback(null, "No orphaned workers found");
+                if (callback !== undefined) {
+                    callback(null, "No orphaned workers found");
+                }
             }
         }
     });
+}
+
+if (LOCAL) {
+    exports.handler();
 }
