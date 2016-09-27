df71db8e2869f306a213167e78915c6ee7dfe762
Dion Amago <dion@transition9.com>
2016-09-27 13:19:20 -0700

    Minor doc change

--- src/haxe/ccc/compute/Definitions.hx
+++ src/haxe/ccc/compute/Definitions.hx
@@ -108,6 +108,7 @@ typedef BasicBatchProcessRequest = {
 	@:optional var contextPath :String;
 	/* Returns the result.json when the job is finished */
 	@:optional var wait :Bool;
+	/* Metadata logged and saved in the job definition and results.json  */
 	@:optional var meta :Dynamic;
 }
 
